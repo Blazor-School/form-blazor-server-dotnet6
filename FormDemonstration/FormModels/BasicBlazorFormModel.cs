d322cc3a1b45da0d06f6eb82d8c8d22f3449360c
Red Wei <hevilhuy2@gmail.com>
2022-02-28 19:54:34 +0700

    Added common mistake examples.

--- FormDemonstration/FormModels/BasicBlazorFormModel.cs
+++ FormDemonstration/FormModels/BasicBlazorFormModel.cs
@@ -1,6 +1,9 @@
-﻿namespace FormDemonstration.FormModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormDemonstration.FormModels;
 
 public class BasicBlazorFormModel
 {
+    [Required]
     public string ExampleString { get; set; } = "Blazor School";
 }
